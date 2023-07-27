e5beae9f8998f2d197b266141125d512f5889cee
SteveNaan <128231912+SteveNaan@users.noreply.github.com>
2023-07-27 13:11:05 +0100

    Form added to add book

--- app.js
+++ app.js
@@ -1,28 +1,45 @@
 let myLibrary = [];
 
-function Book() {
-    const book = {
-        property: 'Value!',
-        otherProperty: 77,
-        "obnoxious property": function() {
-       }
-      }
+function Book(title, author, numberOfPages, read) {
+  this.title = title
+  this.author = author
+  this.numberOfPages = numberOfPages
+  this.read = read
 }
 
-function addBookToLibrary(i) {
-    const newBook = document.createElement("div");
-    newBook.setAttribute("data-index", i)
+function addBookToLibrary(title, author, numberOfPages, read) {
+    let book1 = new Book(title, author, numberOfPages, read)
+    myLibrary.push(book1)
+}
 
-    const title = document.getElementById("title").value;
-     
-    const author = document.getElementById("author").value;
+addBookToLibrary("LOTR", 'Tolkien', 900, true)
 
-    const pages = document.getElementById("pages").value;
+addBookToLibrary("Harry Potter", "JK Rowling", 800, false)
 
-    const read = document.getElementById("read").value;
+console.log(myLibrary)
 
-    container.appendChild(newBook);
-    newBook.classList.add("newBook");
+for(const book of myLibrary) {
+   const books = document.getElementById("Books")
+   var table = document.createElement("table");
+   books.appendChild(table);
+   var tr = table.insertRow()
+   var td = tr.insertCell();
+   td.innerText = book.title;
+   var td = tr.insertCell();
+   td.innerText = book.author
+   var td = tr.insertCell();
+   td.innerText = book.numberOfPages
+   var td = tr.insertCell();
+   td.innerText = book.read
+}
 
-    newBook.appendChild(title)
+var formOpen = false
+
+document.getElementById("newBook").addEventListener('click', function(){
+    var form = document.getElementById("form")
+    form.classList.remove("formClosed")
+})
+
+function bookSubmission(title, author, numberOfPages, read){
+    
 }
